4d0c15fb8983a327a22201227c5711922ca2dea2
realmitza <147737138+realmitza@users.noreply.github.com>
2024-03-09 18:23:43 +0200

    adauga app.py

---
+++ app.py
@@ -0,0 +1,41 @@
+import os
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+from sqlalchemy.sql import func
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+        'sqlite:///' + os.path.join(basedir, 'database.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Firma(db.Model):
+    id = db.Column(db.Integer, pimary_key=True)
+    nume = db.Column(db.String(100), nullable=False)
+    domeniu_activitate = db.Column(db.String(100), nullable=False)
+    adresa = db.Column(db.Column(255))
+    #angajati
+    #functii
+    despre_noi = db.Column(db.Text)
+
+    def __repr__(self):
+        return f'<Firma {self.nume}>'
+
+class Angajat(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nume = db.Column(db.String(100), nullable=False)
+    prenume = db.Column(db.String(100), nullable=False)
+    calificari = db.Column(db.Text)
+    #functie
+
+    def __repr__(self):
+        return f'<Angajat {self.nume}, {self.prenume}>'
+
+
+@app.route('/')
+def index():
+    return 'Test'
